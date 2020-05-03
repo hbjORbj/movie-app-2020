51d54ecb26c5f4edfd58ba5c450a383b85eead2c
hbjORbj <sldisek783@gmail.com>
2020-05-03 18:43:51 +0100

    Updates

---
+++ precache-manifest.5317be4525f7dffb99036ec5f00a588f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43486deb013fac4b8434b707303c5ec0",
+    "url": "/movie-app-2020/index.html"
+  },
+  {
+    "revision": "9f7daf9e76ed385d0710",
+    "url": "/movie-app-2020/static/css/main.b343f84d.chunk.css"
+  },
+  {
+    "revision": "436d070392d0202c3d5d",
+    "url": "/movie-app-2020/static/js/2.3d867733.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-2020/static/js/2.3d867733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7daf9e76ed385d0710",
+    "url": "/movie-app-2020/static/js/main.3247aa40.chunk.js"
+  },
+  {
+    "revision": "07340547d9d1636d5f7b",
+    "url": "/movie-app-2020/static/js/runtime-main.74e7da2c.js"
+  }
+]);

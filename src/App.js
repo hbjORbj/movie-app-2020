cd0e05e47706f127a8874fdae8af175eb2247b1d
hbjORbj <sldisek783@gmail.com>
2020-05-05 14:27:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
-import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
 
@@ -11,7 +10,6 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
